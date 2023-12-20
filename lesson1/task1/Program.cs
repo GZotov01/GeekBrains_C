25c518b33c02197f6a5245246330dbaf78243abf
GZotov <gzotovqq@gmail.com>
2023-12-20 16:58:39 +0300

    add some values lesson1:task1

--- lesson1/task1/Program.cs
+++ lesson1/task1/Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.WriteLine("Hello, World!");
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char C = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(C);
+Console.WriteLine(b);
