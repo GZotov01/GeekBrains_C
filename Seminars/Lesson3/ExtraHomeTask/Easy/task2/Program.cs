7e561b31afd03f282aea6236327b0047779f1fee
GZotov <gzotovqq@gmail.com>
2024-01-19 00:56:58 +0300

    add easy:task2

---
+++ Seminars/Lesson3/ExtraHomeTask/Easy/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите натуральное число:\t");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+int sum = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = i + 1;
+}
+
+foreach (int e in arr)
+{
+    sum += e;
+}
+Console.WriteLine(sum);
