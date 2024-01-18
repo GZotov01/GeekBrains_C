94056e540cbb0964e477b7e923192138ce601179
GZotov <gzotovqq@gmail.com>
2024-01-19 01:42:06 +0300

    add easy:task6

---
+++ Seminars/Lesson3/ExtraHomeTask/Easy/task6/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите натуральное число > 5:\t");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 5)
+{
+    Console.WriteLine("Введенное число < 5");
+}
+else
+{
+    int[] arr = new int[n];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = i + 1;
+    }
+
+    double a, sumA = 0;
+
+    foreach (int e in arr)
+    {
+        sumA += e;
+    }
+    a = sumA / arr.Length;
+
+    double g, sumG = 1;
+
+    foreach (int e in arr)
+    {
+        sumG *= e;
+    }
+    g = Math.Pow(sumG, 1.0 / n);
+
+    Console.WriteLine(Math.Round(a / g, 2));
+}
+
