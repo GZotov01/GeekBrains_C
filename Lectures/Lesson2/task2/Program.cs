98ad2580812aa7768753c075ebf917cdf3212a6e
GZotov <gzotovqq@gmail.com>
2024-01-18 23:21:17 +0300

    add lesson2:task2

--- Lectures/Lesson2/task2/Program.cs
+++ Lectures/Lesson2/task2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter ammount of numbers: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[n];
+int i = 0;
+while (i < n)
+{
+    arr[i] = i + 1;
+    Console.Write($"{arr[i]} ");
+    i++;
+}
