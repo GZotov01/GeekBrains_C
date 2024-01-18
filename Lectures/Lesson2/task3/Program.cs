df91cfa364dab944916ea635de83d3942c7b2934
GZotov <gzotovqq@gmail.com>
2024-01-18 23:41:42 +0300

    add lesson2:task3

---
+++ Lectures/Lesson2/task3/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] arr = new int[10] { 1, 2, 1, 7, 1, 35, 1, 3, 1, 5 };
+
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    else
+    {
+        if (count == (arr.Length - 1))
+        {
+            Console.WriteLine("Четных чисел нет!");
+        }
+        count += 1;
+    }
+    
+}
