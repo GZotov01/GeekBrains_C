30522d6f30eead6796e160ec70d52e74fae4abf2
GZotov <gzotovqq@gmail.com>
2024-01-18 20:45:31 +0300

    Added structure for the repository

---
+++ lesson2/HomeTask/task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+int number = 96;
+int n1, n2;
+
+if (number >= 10 && number <= 99)
+{
+    n1 = number / 10;
+    n2 = number % 10;
+
+    if (n1 > n2)
+    {
+        Console.WriteLine(n1);
+    }
+    else
+    {
+        Console.WriteLine(n2);
+    }
+}
+else
+{
+    Console.WriteLine("Out of range");
+}
