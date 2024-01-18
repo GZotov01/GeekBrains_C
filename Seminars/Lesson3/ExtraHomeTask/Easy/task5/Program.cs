2785e3717c07bca210174dade21126dbb3bde4b6
GZotov <gzotovqq@gmail.com>
2024-01-19 01:14:17 +0300

    add easy:task5

---
+++ Seminars/Lesson3/ExtraHomeTask/Easy/task5/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите натуральное число:\t");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = i + 1;
+}
+
+int sum = 0;
+int count = 0;
+
+foreach (int e in arr)
+{
+    if (n % e == 0)
+    {
+        sum += e;
+        count += 1;
+    }
+}
+Console.WriteLine($"Кол-во делителей: {count}\nСумма делителей: {sum}");
