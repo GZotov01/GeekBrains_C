12ba48b6834ef68fbd0a6780153d72bdbe99756d
GZotov <gzotovqq@gmail.com>
2024-01-18 23:50:44 +0300

    add lesson2:task4

---
+++ Lectures/Lesson2/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] arr = new int[5] { 10, 2, 3, 4, 8 };
+
+int max = arr[0];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+}
+Console.WriteLine($"Наибольшее значение: {max}");
