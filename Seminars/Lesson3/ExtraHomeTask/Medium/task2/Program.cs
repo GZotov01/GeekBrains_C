6c8fa80bd96423fa47ef891eeffa6145414671d9
GZotov <gzotovqq@gmail.com>
2024-01-19 02:50:50 +0300

    add medium:task2

---
+++ Seminars/Lesson3/ExtraHomeTask/Medium/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите натуральное число:\t");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = i + 1;
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] * arr[i] == n)
+    {
+        Console.WriteLine($"Число '{n}' является квадратом числа '{arr[i]}'");
+        break;
+    }
+    else if (i == arr.Length - 1)
+    {
+        Console.WriteLine($"Введеное число '{n}' не является квадратом натурального числа");
+    }
+}
