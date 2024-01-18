4395e570dafd46c24f04b8cca213210e5c8259d1
GZotov <gzotovqq@gmail.com>
2024-01-19 01:02:22 +0300

    add easy:task3

---
+++ Seminars/Lesson3/ExtraHomeTask/Easy/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите натуральное число:\t");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] arr = new double[n];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = Math.Sqrt(i + 1);
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] % 5 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
