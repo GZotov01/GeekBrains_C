dbd9d38b0227bb24e0caa08c9809ad9ddb6fada0
GZotov <gzotovqq@gmail.com>
2024-01-19 03:07:41 +0300

    add medium:task3

---
+++ Seminars/Lesson3/ExtraHomeTask/Medium/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите натуральное число:\t");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = i + 1;
+}
+
+int firstDigit, secondDigit;
+foreach (int e in arr)
+{
+    firstDigit = e / 100;
+    secondDigit = e / 10;
+    if (firstDigit != 0 && secondDigit != 0)
+    {
+        Console.Write($"{e} ");
+    }
+}
