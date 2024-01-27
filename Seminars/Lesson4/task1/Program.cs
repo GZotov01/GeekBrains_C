9e42f46dfab0d63f3622119b977ae3c4ce91bcdb
GZotov <gzotovqq@gmail.com>
2024-01-27 12:42:51 +0300

    add lesson4:task1

---
+++ Seminars/Lesson4/task1/Program.cs
@@ -0,0 +1,42 @@
+﻿int[] CreateArray(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(0, 100);
+        Console.Write($"{arr[i]} ");
+    }
+    return arr;
+}
+bool IsSimple(int n)
+{
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int GetPrimeQuantity(int[] array)
+{
+    int countPrimes = 0;
+
+    foreach (int e in array)
+    {
+        if (IsSimple(e) == true)
+        {
+            countPrimes += 1;
+        }
+    }
+    return countPrimes;
+}
+
+Console.Write("size: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArray(n);
+int result = GetPrimeQuantity(arr);
+Console.WriteLine($"\n{result}");
